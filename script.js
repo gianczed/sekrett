d1fa7b62175cdeaddc9a17800309293a9fbe7da0
gianczed <cedricamoroto12@gmail.com>
2025-02-27 12:04:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,35 @@ let page = 0;
 let typingFinished = false;
 
 const pages = [
-    `Hey you 💕  
-This little space was made just for you.`,
-    `I just wanted to say something important...`,
-    `Every moment with you feels special.`,
-    `You light up the world around you,`,
-    `And make life so much more beautiful.`,
-    `No matter what happens, I'm always here for you.`,
-    `You are loved, appreciated, and cherished. 💖`,
-    `And most of all...`,
-    `I really like you. ❤️`,
-    `Thank you for being you.`
+    `Hii, jo!!!,  
+i made this site for you hehe, ahhm`,
+    `baka sira tong code ko na to`,
+    `wag mo nalang inext pag di pa tapos i type HAHAHHSHA`,
+    `Ay oo nga pala, tapos na yung poem na sinulat ko about you!!`,
+    `and guess what!`,
+    `dito ko nalang ipapabasa sayo ito na 1... 2...3`,
+    `The way you move through this world  
+with kindness in your hands,  
+patience in your voice,  
+and a heart that makes everyone feel safe.`,
+    `I’ve seen so many beautiful things,  
+but nothing compares to you.  
+Not just your face, not just your smile,  
+but the way you make people feel,  
+the way you make me feel—  
+like I’ve finally found home.`,
+    `If one day you feel so lost,  
+Like every road has somehow crossed,  
+And everything feels dark and blue,  
+Don’t worry. I’ll find you.`,
+    `Through rainy days and stormy skies,  
+I’ll be right there, no need for cries.  
+No matter what, no matter where,  
+Just call my name, and I’ll be there.`,
+    `And one thing I really want to say...`,
+    `I really like you.`,
+    `and yeah, I'm always here for you.`,
+    `thank you for reading this!!!`
 ];
 
 function startLoveStory() {
